e9fd70c3f9f60603c257dfa7f3e45c7b91f40e66
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-04-04 05:07:58 -0700

    ifelse logic

--- main.go
+++ main.go
@@ -4,4 +4,19 @@ import "fmt"
 
 func main() {
 	fmt.Println(" Go lang if else")
+
+	loginCount := 9
+	var result string
+
+	if loginCount ==10 {
+
+		result = "Regular user"
+		
+	}else if loginCount >10{
+		result = "Watch out"
+	}else{
+		result = "Exacitiry 10 login count"
+	}
+
+	fmt.Println(result)
 }
